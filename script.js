5f4f98b6431e60b30ca509221e31ab50663e1fbf
mehreenzahid <93670902+mehreenZahid@users.noreply.github.com>
2023-10-12 03:16:31 +0530

    adding numbers

--- script.js
+++ script.js
@@ -2,6 +2,19 @@ const dateOfBirth = document.querySelector("#date-of-birth")
 const luckyNumber = document.querySelector("#lucky-number")
 const checkNumberButton = document.querySelector("#check-number")
 
-checkNumberButton.addEventListener("click",function getValues(){
-    console.log(dateOfBirth.value, luckyNumber.value)
-})
+function checkBirthdayIsLucky(){
+    const dob = date.value
+    const sum = calculateSum(dob)
+    console.log(sum)
+}
+
+function calculateSum(dob){
+    dob = dob.replaceAll("-","")
+    let sum = 0;
+    for(let index=0; index < dob.length; index++){
+        sum = sum + Number(dob.charAt(index))
+    }
+    return sum;
+}
+
+checkNumberButton.addEventListener("click", checkBirthdayIsLucky)
